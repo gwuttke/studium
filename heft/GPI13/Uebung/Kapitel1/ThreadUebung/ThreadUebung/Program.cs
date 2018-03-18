17bc0b674144f18262a1d97fe1443919705bd455
Georg Wuttke <georg1wuttke@gmail.com>
2018-03-18 13:38:37 +0100

    Thread Benutzereingabe (GPI13 S. 12)

---
+++ heft/GPI13/Uebung/Kapitel1/ThreadUebung/ThreadUebung/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    private  static char ZEICHEN = '&';
+    static void Main(string[] args)
+    {
+        Program p = new Program();
+        Thread lesen = new Thread(new ThreadStart(p.eingaben));
+        Thread drucken = new Thread(new ThreadStart(p.drucken));
+        lesen.Name = "lesen";
+        drucken.Name = "drucken";
+        Console.WriteLine("Es wird immer ein Thread auf Ihre Eingabe warten mi einem Zeichen,");
+        Console.WriteLine("Ein anderer wird immer dass Zeichen {0} ausgeben, bis Sie ein Zeichen eingegeben haben welches dann ausgegeben wird",ZEICHEN);
+
+        lesen.Start();
+        drucken.Start();
+
+    }
+
+    private void eingaben()
+    {
+        do
+        {
+            ZEICHEN = Console.ReadKey().KeyChar;
+        } while (true);
+    }
+
+    private void drucken()
+    {
+        do
+        {
+            Console.Write(ZEICHEN);
+            // damit das Programm nicht zu schnell ausgeführt wird
+            Thread.Sleep(5);
+        } while (true);
+    }
+}
