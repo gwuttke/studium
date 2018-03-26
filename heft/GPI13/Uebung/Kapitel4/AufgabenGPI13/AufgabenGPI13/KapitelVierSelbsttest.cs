1879e020cc9050282e92e4b0ca643b5b81d8a90e
Georg Wuttke <georg1wuttke@gmail.com>
2018-03-26 18:29:46 +0200

    List Join To String (Kapitel 4 Aufgaben zum Selbsttest 1)

---
+++ heft/GPI13/Uebung/Kapitel4/AufgabenGPI13/AufgabenGPI13/KapitelVierSelbsttest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AufgabenGPI13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sList = new List<string>();
+            sList.Add("Hallo");
+            sList.Add("Georg");
+            sList.Add("Wie");
+            sList.Add("Geht");
+            sList.Add("es");
+            sList.Add("dir");
+            Console.WriteLine(listToString(sList));
+            Console.ReadKey();
+        }
+
+        protected static string listToString(List<string> listStrings)
+        {
+            listStrings.Reverse();
+            return String.Join(" ", listStrings);
+        }
+    }
+}
